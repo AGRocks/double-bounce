6ff6a0b9ad67c31c21fa6103613119c4ac6af7be
agrocks <gabuzaav@gmail.com>
2018-01-04 11:07:33 +0100

    cleaning up; added xamarin project

---
+++ traininghub.api/Controllers/GamesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traininghub.Data;
+
+namespace traininghub.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class GamesController : Controller
+    {
+
+        public GamesController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Game> GetGames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
